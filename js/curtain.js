4af29c59dfdc2220ffb9845a38af18f199d29cf9
KantaroNakaya <shimeno38saiko@gmail.com>
2025-01-04 14:29:45 +0900

    pc時　カーテンが開くと同時に左右の要素も表示するように修正

--- js/curtain.js
+++ js/curtain.js
@@ -3,6 +3,7 @@
     $(window).on('load', function() {
         setTimeout(function() {
             $('.mv').addClass('appear');
+            $('.pc-logo,.pc-link').addClass('d-flex');
         }, 4000);
     });
 })(jQuery);
